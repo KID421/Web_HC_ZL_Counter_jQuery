e4ad47fd43636ac3d2cf536c1013de996ee8924d
KID421 <suchild7@gmail.com>
2020-03-15 16:57:13 +0800

    完成計數器效果

--- counter.js
+++ counter.js
@@ -2,10 +2,22 @@
 // attr 取得屬性
 $("*").each(function() {
     var number = $(this).attr("data-ct-number");
-    var duration = $(this).attr("data-ct-duration");
+    var dur = $(this).attr("data-ct-duration");
 
-    console.log(number);
-    console.log(duration);
-
-   
+    // 此元素.動畫(屬性物件，設定物件)
+    $(this).animate({
+        // 記數：指定數字
+        countNum: number
+    }, {
+        // 時間
+        duration: dur,
+        // 緩動
+        easing: "linear",
+        // 每一步
+        step: function(now){
+            // text() 更新文字
+            // Math.floor() 去小數點
+            $(this).text(Math.floor(now))
+        }
+    });
 })
